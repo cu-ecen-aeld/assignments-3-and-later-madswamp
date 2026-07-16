9cd2d5aeb97b59f5467188148bfc56e35ebe6eb7
Jose Antunes <jose.antunes@tii.ae>
2026-07-16 17:02:52 +0400

    Added the start-stop-daemon script for the assignment 5

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+DAEMON="/usr/bin/aesdsocket"
+DAEMON_ARGS="-d"
+STOP_SIGNAL="TERM"
+
+case "$1" in
+    start)
+        echo "Starting $DAEMON..."
+        start-stop-daemon --start --exec "$DAEMON" -- "$DAEMON_ARGS"
+        ;;
+    stop)
+        echo "Stopping $DAEMON..."
+        start-stop-daemon --stop --exec "$DAEMON" --signal "$STOP_SIGNAL"
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
